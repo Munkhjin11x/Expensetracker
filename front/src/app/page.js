52c01b78e3b2e09663e7a55ff102b3df033f85c4
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2023-12-26 23:30:33 +0800

    s

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,8 +1,9 @@
 
 export default function Home() {
   return (
-    <main >
-    
+    <main className="flex" >
+    <h1 className=" flex font-bold">dsajdlsakjd;l</h1>
+    <h2>dasdsadas</h2>
     </main>
   )
 }
